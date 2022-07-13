265be2dfd0383bda4d7fc5167a1f72b7b6ebb1ae
wandera7 <julius.wandera@student.moringaschool.com>
2022-07-13 14:26:45 +0300

    Done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -2,7 +2,7 @@ import React from "react";
 import howManyParks from  "./parks/howManyParks";
 
 function ColoradoStateParks() {
-  howManyParks();  // => "42 parks!"
+  howManyParks();// => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
